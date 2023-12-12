318ee1a92ff1ddf6cdebdfdb8b5bc6fac1656c86
Misha Sysoev <54980483+Lamass566@users.noreply.github.com>
2023-12-13 00:15:12 +0200

    fixed bad, added modal

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -2,8 +2,16 @@
 
 import { AiOutlineMenu } from "react-icons/ai"
 import Avatar from "../Avatar";
+import { useCallback, useState } from "react";
+import MenuItem from "./MenuItem";
 
 const UserMenu = () => {
+    const [isOpen, SetIsOpen] = useState(false)
+
+    const toggleOpen = useCallback(() => {
+        SetIsOpen((value) => !value)
+    }, [])
+
     return(
         <div className="relative">
             <div className="flex
@@ -25,7 +33,7 @@ const UserMenu = () => {
                                 >
                     Airbnb your home
                 </div>
-                <div onClick={()=>{}} 
+                <div onClick={toggleOpen} 
                      className="p-4 
                                 md:py-1 
                                 md:px-2 
@@ -46,6 +54,32 @@ const UserMenu = () => {
                     </div>
                 </div>
             </div>
+            {isOpen && (
+                <div className="absolute 
+                                rounded-xl
+                                shadow-md
+                                w-[40vw]
+                                md:w-3/4
+                                bg-white
+                                overflow-hidden
+                                right-0
+                                top-12
+                                text-sm"
+                >
+                    <div className="flex flex-col cursor-pointer">
+                        <>
+                            <MenuItem 
+                                onClick={()=>{}}
+                                label="Login"
+                            />
+                            <MenuItem 
+                                onClick={()=>{}}
+                                label="Sign up"
+                            />
+                        </>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
